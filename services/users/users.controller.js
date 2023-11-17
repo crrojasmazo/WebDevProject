c0ec7db649c892bc0d4696afb52723e90d9f2679
crojas <crrojasm@outlook.com>
2023-11-16 23:31:23 -0500

    user endpoint working properly

--- services/users/users.controller.js
+++ services/users/users.controller.js
@@ -7,18 +7,18 @@ const getUsers = async (_req, res) => {
 
 const addUsers = async (req, res ) => {
     const new_user = await User.create( req.body )
-    res.send( new_user)
+    res.status(201).send( new_user)
 }
 
 const updateUsers = async (req, res ) => {
-    const updated_user = await User.findOneAndUpdate( req.params.id, req.body, {new: true} )
-    res.send( updated_user)
+    const updated_user = await User.findOneAndUpdate( { _id: req.params.id }, req.body, {new: true} )
+    res.status(200).send( updated_user)
 }
 
 const deleteUsers = async (req, res ) => {
     const user_id = req.params.id
-    await User.findByIdAndRemove( user_id )
-    res.send(`user ${user_id} was deleted`)
+    await User.findOneAndDelete(user_id)
+    res.status(204).end()
 }
 
 module.exports = {
